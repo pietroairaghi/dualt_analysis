f3d675216e931d4f9e9a61c7578fb2a508ac96b3
Pietro Airaghi <pairaghi@hotmail.it>
2020-05-15 14:53:42 +0200

    users and activities pt1

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 -- N Activities per user
 CREATE OR REPLACE VIEW n_activities AS (
 	SELECT us_user, COUNT(ac_activity) as n_activities FROM users
-		NATURAL JOIN activities_users
+		NATURAL JOIN activities_users 
+		NATURAL JOIN activities_versions
+		WHERE acv_type = 'final'
 		GROUP BY us_user
 );
 	
@@ -116,6 +118,7 @@ SELECT users.us_user as user_id,us_archived as archived,user_type,classes, compa
 	n_in_curriculum_semester1,n_in_curriculum_semester2,
 	n_in_curriculum_semester3,n_in_curriculum_semester4,n_in_curriculum_semester5,
 	n_feedback_requests,n_received_feedback_responses,n_received_feedback_requests,n_feedback_responses,
+	avg_activity_evaluations, avg_reflection_length, avg_specific_evaluations
 	n_files,n_folders
 FROM `users` 
 
@@ -134,6 +137,14 @@ FROM `users`
 	NATURAL LEFT JOIN (
 		SELECT us_user, COUNT(*) as total_logins FROM LOG_logins GROUP BY us_user
 	) AS T_logins
+	
+	NATURAL LEFT JOIN(
+		SELECT us_user, ROUND(AVG(LENGTH(ref_reflection))) AS avg_reflection_length, AVG(ref_evaluation) AS avg_specific_evaluations FROM reflections GROUP BY us_user
+	) AS T_reflection_details
+	
+	NATURAL LEFT JOIN(
+		SELECT us_user, AVG(au_evaluation) AS avg_activity_evaluations FROM activities_users GROUP BY us_user
+	) AS T_avg_activity_evals
 	
 	NATURAL LEFT JOIN n_activities
 	NATURAL LEFT JOIN n_recipes
@@ -157,4 +168,59 @@ FROM `users`
 	) as T_folders
 	
 WHERE users.us_user>20 ;
+
+
+
+
+
+
+
+
+
+
+
+
+-- STEPS info BY ACTIVITY
+CREATE OR REPLACE VIEW activity_steps_info AS (
+	SELECT st_idStep, ac_activity, ROUND(AVG(LENGTH(st_description))) AS avg_step_len, COUNT(*) AS n_steps 
+	FROM steps 
+		WHERE LENGTH(st_description) > 0 GROUP BY ac_activity
+);
+
+
+-- ACTIVITIES INFO
+SELECT ac_activity AS activity_id, us_user AS user_id, at_activityType, LENGTH(ac_description) AS len_description, 
+		ac_atSchool, ac_atInteraziendale, (1-ac_atSchool-ac_atInteraziendale) AS ac_atCompany, avg_step_len, n_steps,
+		au_evaluation AS user_evaluation, start_date, ac_date AS end_date, (start_date-ac_date) AS edit_period,
+		avg_specific_evaluations, avg_reflection_length, in_curriculum
+FROM `activities` NATURAL JOIN activities_versions NATURAL JOIN activities_users
+
+	NATURAL LEFT JOIN (
+		SELECT ac_activity_ref AS ac_activity, MIN(ac_date) AS start_date FROM activities_versions NATURAL JOIN activities WHERE acv_type = 'autosave' GROUP BY ac_activity_ref
+	) AS T_start_date
+	
+	NATURAL LEFT JOIN (
+		SELECT ac_activity, us_user, ROUND(AVG(LENGTH(ref_reflection))) AS avg_reflection_length, AVG(ref_evaluation) AS avg_specific_evaluations FROM reflections GROUP BY ac_activity,us_user
+	) AS T_n_reflections
+	
+	NATURAL LEFT JOIN(
+		SELECT ac_activity, COUNT(fi_file) AS n_images FROM files_steps NATURAL JOIN steps GROUP BY ac_activity
+	) AS T_n_files
+	
+	NATURAL LEFT JOIN(
+		SELECT ac_activity, atcl_classificationType, 1 AS in_curriculum FROM activities NATURAL LEFT JOIN activities_classification
+			WHERE atcl_classificationType = 'inCurriculum'
+	) AS T_check_curriculum
+
+	NATURAL LEFT JOIN activity_steps_info
+	
+WHERE acv_type = 'final';
+
+
+
+
+
+
+
+
 
